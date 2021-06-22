baa16d894ebe662360b91c7d1628c8a280f4bfa3
Deore Rameshwari Ravindra <rameshwarideore2018@gmail.com>
2021-06-22 13:31:14 +0530

    Update ConvertToBinary.java

--- src/main/java/exam/ConvertToBinary.java
+++ src/main/java/exam/ConvertToBinary.java
@@ -4,6 +4,32 @@ public class ConvertToBinary {
 
   public String toBinary(final int n) {
     //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-    return "";
+    
+    int a=n;
+      int[] bno = new int [32];
+      String res=null;
+      int i=0;
+      while(a>0)
+      {
+          bno[i]=a%2;
+          a=a/2;
+          i++;
+      }
+    res=Arrays.toString(bno);
+    return res;
   }
-}
+    public static void main(String args[]) {
+        Scanner sc= new Scanner(System.in);   
+        int n=sc.nextInt();
+        String number=null;
+        number=toBinary(n);
+        int j=number.length();
+      for( j=j-1;j>=0;j--)
+      {
+        System.out.println(number[j]);
+      }
+        
+    }
+
+  
+
